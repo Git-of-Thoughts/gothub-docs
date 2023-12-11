9df6ab789a9e4465ad834ea2c49fe6e610fb8de5
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-12-11 02:08:33 -0800

    change

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>GotHub</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/Git-of-Thoughts/GotHub-JupyterHub-Home",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com/invite/9rq9CgVvbD",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "GotHub.AI © 2023",
   },
-}
+};
 
-export default config
+export default config;
